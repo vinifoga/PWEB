01317de842a31b6de74b26f35086c79fd71ab0ed
vinifoga <59516848+vinifoga@users.noreply.github.com>
2020-10-28 21:02:19 -0300

    createAssignment

---
+++ Atividade10/functions.js
@@ -0,0 +1,9 @@
+function abrirJanela(){
+    document.getElementById("janela").src="_images/janelaaberta.png";
+}
+function fecharJanela(){
+    document.getElementById("janela").src="_images/janelafechada.png";
+}
+function quebrarJanela(){
+    document.getElementById("janela").src="_images/janelaquebra.png";
+}
